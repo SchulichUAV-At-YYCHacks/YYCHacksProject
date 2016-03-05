d5a8d8990c4ded5d977ad5417694f8f885a69e92
masonbrothers <masonbrothers@users.noreply.github.com>
2016-03-05 09:56:09 -0700

    Having problems getting Geocoding to work. Need to change http to https

--- backend.js
+++ backend.js
@@ -1,10 +1,57 @@
 //NodeJS
 
 var http = require('http');
+var https = require('https');
 var fs = require('fs');
 var portNo = 8081;
 var GOOGLE_GEOCODE_API_KEY = "YOUR_API_KEY";
 
+function getGeoCode(address){
+	fs.readFile('geocodeAPIKey.log', function(err, contents) {
+		GOOGLE_GEOCODE_API_KEY = contents;
+		console.log(contents);
+
+
+
+
+		//var address = "1600 Amphitheatre Parkway, Mountain View, CA"
+
+
+		var google_api_path = '/maps/api/geocode/json?address=' + address.split(' ').join('+') + '&key=' + GOOGLE_GEOCODE_API_KEY;
+
+		console.log(google_api_path);
+
+		var options = {
+			host: 'maps.googleapis.com',
+			port: 80,
+			path: google_api_path
+		};
+
+		console.log(options.path);
+
+		var google_api_url = "https://" + options.host + google_api_path;
+
+
+		console.log(google_api_url);
+		http.get(options, function(resource) {
+			//console.log(resource);
+			var body = '';
+			resource.on('data', function(packet){
+				body += packet;
+			});
+
+			resource.on('end', function(){
+				var googleMapsData = JSON.parse(body);
+				console.log(googleMapsData);
+			});
+		}).on('error', function(e){
+			console.log("Error in loading Geocode: ", e)
+		});
+
+	});
+}
+
+getGeoCode("1600 Amphitheatre Parkway, Mountain View, CA")
 
 http.createServer(function(request, response) {
     response.writeHead(200);
@@ -19,41 +66,6 @@ http.createServer(function(request, response) {
 
 console.log('Check Port ' + portNo + '....');
 
-
-
-
-
-var address = "1600 Amphitheatre Parkway, Mountain View, CA"
-
-
-var google_api_path = '/maps/api/geocode/json?address=' + address.split(' ').join('+') + '&key=' + GOOGLE_GEOCODE_API_KEY;
-
-console.log(google_api_path);
-
-var options = {
-	host: 'maps.googleapis.com',
-	port: 80,
-	path: google_api_path
-};
-
-console.log(options.path);
-
-var google_api_url = "https://" + options.host + google_api_path;
-
-
-console.log(google_api_url);
-http.get(options, function(resource) {
-	//console.log(resource);
-	var body = '';
-	resource.on('data', function(packet){
-		body += packet;
-	});
-
-	resource.on('end', function(){
-		var googleMapsData = JSON.parse(body);
-		console.log(googleMapsData);
-	});
-});
 
 
 
